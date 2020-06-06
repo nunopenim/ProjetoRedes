4ac8e755c6195d3d4c32c33a4c1989fbeb539e34
Nuno Penim <nuno.penim@gmail.com>
2020-06-06 12:04:10 +0100

    Cliente: added menu

--- src/Cliente.java
+++ src/Cliente.java
@@ -6,6 +6,25 @@ import java.net.*;
 
 public class Cliente {
 
+    public static boolean isNumber(String s) {
+        try{
+            int number = Integer.parseInt(s);
+        }
+        catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean isValueInArray(int valToCheck, int[] array){
+        for (int val : array) {
+            if (val == valToCheck) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public static class PrintingThread implements Runnable {
         String text;
         public PrintingThread(String text) {
@@ -110,11 +129,40 @@ public class Cliente {
 
     }
 
-    public static void main(String[] args) throws SocketException, UnknownHostException {
-        Thread teste1 = new Thread(new UDPConnection("localhost", 9031));
+    public static int menu() throws IOException {
+        boolean invalid = true;
+        int option = 0;
+        int optionsArray[] = {0, 1, 2, 3, 4, 5, 99};
+        while (invalid) {
+            System.out.println("MENU CLIENTE\n");
+            System.out.println("0 - Menu Inicial");
+            System.out.println("1 - Listar utilizadores online");
+            System.out.println("2 - Enviar mensagem a um utilizador");
+            System.out.println("3 - Enviar mensagem a todos os utilizadores");
+            System.out.println("4 - Lista branca de utilizadores");
+            System.out.println("5 - Lista negra de utilizadores");
+            System.out.println("99 - Sair");
+            BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
+            String s = null;
+            s = bufferRead.readLine();
+            if (isNumber(s) && isValueInArray(Integer.parseInt(s), optionsArray)) {
+                option = Integer.parseInt(s);
+                invalid = false;
+            }
+            else {
+                System.out.println("Opção inválida!");
+                invalid = true;
+            }
+        }
+        return option;
+    }
+
+    public static void main(String[] args) throws IOException {
+        /*Thread teste1 = new Thread(new UDPConnection("localhost", 9031));
         Thread teste2 = new Thread(new TCPConnection("localhost", 6500));
         teste1.start();
-        teste2.start();
+        teste2.start();*/
+        menu();
     }
 
 
